32bfc780f826c583d383cde9c7285d51ecdebd47
SantiagoPunales <santiago.punales@correo.ucu.edu.uy>
2024-08-21 10:36:01 -0300

    Menu hecho por seysa

--- src/Menu.cs
+++ src/Menu.cs
@@ -1,3 +1,7 @@
+// using System.Linq;
+
+using System.Net.Security;
+
 namespace Ucu.Poo.Restaurant;
 
 using System.Collections;
@@ -9,4 +13,26 @@ using System.Collections;
 public class Menu
 {
     private ArrayList dishes = new ArrayList();
-}
+    public void AddDish(Dish dish)
+    {
+        this.dishes.Add(dish);
+    }
+    public void RemoveDish(Dish dish)
+    {
+        this.dishes.Remove(dish);
+    }
+
+    public Dish GetDishByName(string name)
+    {
+        Dish resultado = null;
+        foreach (Dish dish in dishes)
+        {
+            if (dish.Name == name)
+            {
+                resultado = dish;
+            }
+        }
+
+        return resultado;
+    }
+}   
